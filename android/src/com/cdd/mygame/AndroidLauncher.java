9b16a629ef03f40a9a8bf9d0279a4d79fba9dca8
MrunalrajRedij <Mrunalrajredij@gmail.com>
2021-02-18 20:02:01 +0530

    first commit!

---
+++ android/src/com/cdd/mygame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cdd.mygame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cdd.mygame.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}

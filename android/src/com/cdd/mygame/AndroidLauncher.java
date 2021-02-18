a89714f72d55e764a09d7e2404ee71a4d386e826
MrunalrajRedij <Mrunalrajredij@gmail.com>
2021-02-18 21:29:20 +0530

    second commit!

--- android/src/com/cdd/mygame/AndroidLauncher.java
+++ android/src/com/cdd/mygame/AndroidLauncher.java
@@ -1,16 +1,15 @@
 package com.cdd.mygame;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.cdd.mygame.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
 		initialize(new MyGdxGame(), config);
 	}
 }
--- core/src/com/cdd/mygame/MyGdxGame.java
+++ core/src/com/cdd/mygame/MyGdxGame.java
@@ -5,15 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class MyGdxGame extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
+	Rectangle imgO;
 	
 	@Override
 	public void create () {
+		//initialization
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		imgO = new Rectangle();
 	}
 
 	@Override
@@ -21,8 +27,18 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, imgO.x, imgO.y);
 		batch.end();
+
+
+		//touch mechanism
+		if(Gdx.input.isTouched()){
+			Vector2 touch = new Vector2();
+			touch.set(Gdx.input.getX(),Gdx.input.getY());
+			imgO.x = touch.x -110;
+			imgO.y = -touch.y + 940;
+		}
+
 	}
 	
 	@Override
